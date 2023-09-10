e536acd857b5c85143903c64b309d36f768d0fc0
eldapour <abdallahmahmoud1422003@gmail.com>
2023-09-10 14:10:12 +0300

    finish 10-9-2023

--- resources/views/admin/warehouse/parts/create.blade.php
+++ resources/views/admin/warehouse/parts/create.blade.php
@@ -40,6 +40,7 @@
             </div>
         </div>
 
+
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">اغلاق</button>
             <button type="submit" class="btn btn-primary" id="addButton">اضافة</button>
