b975c200815eeb6540cbb0d723aa7c8c5cbd1544
eldapour <abdallahmahmoud1422003@gmail.com>
2023-09-12 16:05:37 +0300

    add_change_status_button_12_9_2023

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -18,22 +18,8 @@ class SettingSeeder extends Seeder
                 'name_ar' => 'ان تي اكسبريس',
                 'name_en' => 'NTexpress',
                 'logo' => 'uploads/settings/logo.png',
-                'conditions_ar' => 'شروط وأحكام النقل :  يحظر شحن النقود والذهب والأدوية وما ينسى النظام على منعه
-الناقل غير مسؤول عن أي اضرار او تلف لاي بساحة قابلة للكسر الهاء التحميل أو الطبيعية بالحراسة المرورية او الحريق او الكرا
-القصب تمويلي عن فقدان اي ارسالية قيمة الارسالية الخاصة بناقل أو (100) مائة ريال عالم باقي على غير ذلك
-محتوى اي ارسالية النقل غير مسؤولة
-الناقل غير مسؤول عن أي ارسالية لم يستلمها المرسل
-ستلام والتوقيع نسخة الوثيقة القبولاً بكافة شروط النقل
-الحق في التعليق في إرساليات يتضح الناقل أنها التهاب شروط النقل
-حدة النقل المثقل عليها خمسة أيام، ولا يحق للموصل تقديم شكوى قبل لتلك الفترة',
-                'conditions_en' => 'Transportation Terms & Conditions : 1. is forbidden to ship cash, gold and medicines What the system atipulates la impermissible
-2. The carrier is not responsible for any damage or damage to any goods subject to breakage during loading traffic accidents, fire or natural disasters
-3. Maximum compensation for the loss of any consignment, the value of the consignment belonging to a carrier, or a maximum of (100) one hundred rats, unless otherwise agreed.
-4. Transport is not responsible for the content of any consignment.
-5. The carrier is not responsible for any consignment that has not been received by the addressee and has not been received by the sender after the lapse of twenty-one(21) days.
-6. Receiving and Signing the policy copy as acceptance of all the Transport conditions.
-7. The right to suspend any consignments that the carrier finds to be in violation of the Conditions of Carriage.
-B. The agreed period of transport is five days, and the sender has no right to complain before that period',
+                'conditions_ar' => '<p>1-يحظر شحن النقود والذهب والادوية وما ينص النظام على منعه<br>2-الناقل غير مسئول عن اى اضرار او تلف لا بضاعة قابلة للكسر اثناء التحميل او الحوادث المرورية او الحريق او الكوارث الطبيعية<br>3-أقصى تعويض عن فقدان اى ارسالية قيمة الارسالية الخاصة بناقل او بحد اقصى (100) مائة ريال ما لم يتفق على غير ذلك<br>4- النقل غير مسئول عن محتوى اى ارسالية<br>5- الناقل غير مسئول عن اى ارسالية لم يستلمها المرسل اليه ولم يستقبلها المرسل بعد مرور 21 احدى وعشرون يوما<br>6- استلام وتوقيع نسخة الوثيقة قبولا بكافة شروط النقل<br>7- الحق فى تعليق اى ارساليات يتضح الناقل انها تنتهك شروط النقل<br>8- مدة النقل المتفق عليها خمسة ايام ولا يحق للمرسل تقديم شكوى قبل تلك الفترة<br>&nbsp;</p>',
+                'conditions_en' => '<p>1- It is prohibited to ship money, gold, medicines, and anything prohibited by the system<br>2-The carrier is not responsible for any damage or damage to breakable goods during loading, traffic accidents, fire, or natural disasters.<br>3- The maximum compensation for the loss of any consignment is the value of the carrier’s consignment, or a maximum of (100) one hundred riyals, unless otherwise agreed upon.<br>4- Transport is not responsible for the content of any shipment<br>5- The carrier is not responsible for any shipment that the addressee has not received and the sender has not received after 21 twenty-one days.<br>6- Receive and sign a copy of the document accepting all conditions of transportation<br>7- The right to suspend any shipments that the carrier finds to be in violation of the conditions of transportation<br>8- The agreed upon transportation period is five days, and the sender does not have the right to file a complaint before that period</p>',
                 'shipment_price' => 30,
             ]);
     }
